5f510f8f7d7eb3860990d4645d2499ee2fc37e1e
jiaowh <jiaowh@hotmail.com>
2023-12-24 18:46:05 +0900

    added follow movement
    they kinda follow mouse?
    need to add random movements too

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
-
+var fish1,fish2,fish3,fish4,fish5;
 // obtain window size
 const windowWidth = window.innerWidth;
 const windowHeight = window.innerHeight;
@@ -26,7 +26,7 @@ scene.add(gridHelper);
 // camera
 //change position to change view
 const camera = new THREE.PerspectiveCamera(75, windowWidth / windowHeight, 0.1, 1000);
-camera.position.set(5, 2, 0);
+camera.position.set(0, 2, 4);
 camera.lookAt(0, 0, 0);
 
 // light
@@ -42,37 +42,38 @@ const controls = new OrbitControls(camera, renderer.domElement);
 // fish models
 const loader = new GLTFLoader();
 loader.load('/fish.glb', function (gltf) {
-    const model = gltf.scene;
-    model.scale.set(2.0, 2.0, 2.0);
-	model.position.set(1.0,1.0,1.0)
-    scene.add(model);
+    fish1 = gltf.scene;
+    fish1.scale.set(2.0, 2.0, 2.0);
+	fish1.position.set(1.0,1.0,1.0)
+    scene.add(fish1);
 	
 });
 loader.load('/fish.glb', function (gltf) {
-    const model = gltf.scene;
-    model.scale.set(2.0, 2.0, 2.0);
-	model.position.set(2.0,1.0,1.0)
-    scene.add(model);
+    fish2 = gltf.scene;
+    fish2.scale.set(2.0, 2.0, 2.0);
+	fish2.position.set(2.0,1.0,1.0)
+    scene.add(fish2);
 });
 loader.load('/fish.glb', function (gltf) {
-    const model = gltf.scene;
-    model.scale.set(2.0, 2.0, 2.0);
-	model.position.set(2.0,2.0,1.0)
-    scene.add(model);
-});
-
-loader.load('/fish.glb', function (gltf) {
-    const model = gltf.scene;
-    model.scale.set(2.0, 2.0, 2.0);
-	model.position.set(2.0,1.0,2.0)
-    scene.add(model);
+    fish3 = gltf.scene;
+    fish3.scale.set(2.0, 2.0, 2.0);
+	fish3.position.set(2.0,2.0,1.0)
+    scene.add(fish3);
 });
 
 loader.load('/fish.glb', function (gltf) {
-    const model = gltf.scene;
-    model.scale.set(2.0, 2.0, 2.0);
-	model.position.set(1.5,1.5,1.5)
-    scene.add(model);
+    fish4 = gltf.scene;
+    fish4.scale.set(2.0, 2.0, 2.0);
+	fish4.position.set(2.0,1.0,2.0)
+    scene.add(fish4);
+});
+
+loader.load('/fish.glb', function (gltf) {
+    fish5 = gltf.scene;
+    fish5.scale.set(2.0, 2.0, 2.0);
+	fish5.position.set(1.5,1.5,1.5)
+    scene.add(fish5);
+	
 });
 
 
@@ -82,3 +83,32 @@ function animate() {
     renderer.render(scene, camera);
 }
 animate();
+//mouse variable
+var mouse = {x: 0, y: 0};
+document.addEventListener('mousemove', onMouseMove, false);
+
+function onMouseMove(event) {
+
+    // Update the mouse variable
+    event.preventDefault();
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
+	var vector = new THREE.Vector3(mouse.x, mouse.y, 0.5);
+	vector.unproject( camera );
+	var dir = vector.sub( camera.position ).normalize();
+	var distance = - camera.position.z / dir.z;
+	var pos = camera.position.clone().add( dir.multiplyScalar( distance ) );
+	console.log(pos);
+	fish1.position.copy(pos);
+	pos.x+=0.5;
+	pos.y+=0.5;
+	fish2.position.copy(pos);
+	pos.x+=0.5;
+	fish3.position.copy(pos);
+	pos.y+=0.5;
+	fish4.position.copy(pos);
+	pos.x+=0.5;
+	pos.y+=0.5;
+	fish5.position.copy(pos);
+
+};
